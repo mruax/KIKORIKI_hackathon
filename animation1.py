6d61a3e716e081f4549ae5c1a03e4766c6c906ad
jonyks <146552678+jonyks@users.noreply.github.com>
2024-05-23 22:31:40 +0500

    Update animation1.py

--- animation1.py
+++ animation1.py
@@ -1,13 +1,13 @@
 import sys
 
-from PySide6.QtCore import QRectF, Qt, QPropertyAnimation, QPoint
+from PySide6.QtCore import QRectF, Qt, QPropertyAnimation, QPoint, QTimer
 from PySide6.QtGui import QPainter, QPixmap
 from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
 from PySide6.QtSvg import QSvgRenderer
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
+        global pixmap
         super().__init__()
         self.title = "Анимация картинки SVG"
         self.setWindowTitle(self.title)
@@ -26,6 +26,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.label)
         self.resize(1000, 1000)
 
+        QTimer.singleShot(8000, self.start_hide_animation)  # Start hide animation after 8 seconds
+    def start_hide_animation(self):
+        self.anim.setStartValue(QPoint(0, 0))
+        self.anim.setEndValue(QPoint(-pixmap.width(), 0))
+        self.anim.start()
+
     def render_svg(self, width, height):
         pixmap = QPixmap(width, height)
         pixmap.fill(Qt.transparent)
@@ -38,7 +44,6 @@ class MainWindow(QMainWindow):
         painter.end()
 
         return pixmap
-
 
 app = QApplication(sys.argv)
 w = MainWindow()
