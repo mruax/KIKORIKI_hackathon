2c7b153c0f546d4cfabc221d722315a0eec17b0b
jonyks <146552678+jonyks@users.noreply.github.com>
2024-05-23 21:35:42 +0500

    Update animation.py

--- animation.py
+++ animation.py
@@ -1,22 +1,29 @@
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtCore import QPropertyAnimation, QPoint, QEasingCurve
+import sys
 
-class Window(QWidget):
+from PySide6.QtCore import QPoint, QPropertyAnimation
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QMainWindow, QApplication, QLabel
+
+class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.resize(600, 600)
-        self.child = QWidget(self)
-        self.child.setStyleSheet("background-color:red;border-radius:15px;")
-        self.child.resize(100, 100)
-        self.anim = QPropertyAnimation(self.child, b"pos")
-        self.anim.setEasingCurve(QEasingCurve.OutInCubic)
-        self.anim.setEndValue(QPoint(400, 400))
-        self.anim.setDuration(1500)
+        super(MainWindow, self).__init__()
+        self.title = "Анимация картинки"
+        self.setWindowTitle(self.title)
+
+        self.label = QLabel(self)
+        self.pixmap = QPixmap('cat.jpg')
+        self.label.setPixmap(self.pixmap)
+        self.label.setStyleSheet("border: 2px solid white; border-radius: 10px;")
+        self.setCentralWidget(self.label)
+        self.resize(1000,1000)
+
+        self.anim = QPropertyAnimation(self.label, b"pos")
+        self.anim.setStartValue(QPoint(0, 0))
+        self.anim.setEndValue(QPoint(self.width() - self.pixmap.width(), self.height() - self.pixmap.height()))
+        self.anim.setDuration(2000)
         self.anim.start()
 
-
-if __name__ == "__main__":
-    app = QApplication([])
-    window = Window()
-    window.show()
-    app.exec()
+app = QApplication(sys.argv)
+w = MainWindow()
+w.show()
+sys.exit(app.exec())
