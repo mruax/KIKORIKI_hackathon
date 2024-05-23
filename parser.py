9f18d20935433873a376c83b6239653ddfc5c7c6
mruax <matvey.chernyakov@yandex.ru>
2024-05-23 19:58:44 +0500

    Updated parser

--- parser.py
+++ parser.py
@@ -1,22 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL страницы, которую будем парсить
-url = 'https://k-telecom.org/oborudovanie/'
+url = "https://k-telecom.org/oborudovanie/"
 
-# Делаем HTTP GET запрос к указанному URL
 response = requests.get(url)
 
-# Проверяем, успешен ли запрос (статус код 200)
 if response.status_code == 200:
-    # Парсим HTML содержимое страницы с помощью BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, "html.parser")
+    popular_items_html = soup.find_all("a", class_="product-card__wrapper")
 
-    # Находим все элементы h2 (или другие нужные элементы)
-    headlines = soup.find_all('h2')
+    for number, item_block in enumerate(popular_items_html):
+        name = item_block.find("h6", class_="product-card__title h6").get_text().rstrip("\n")
+        description = item_block.find("div", class_="product-card__desc")
+        price = item_block.find("div", class_="product-card__price h5").get_text()
 
-    # Выводим текст каждого заголовка
-    for headline in headlines:
-        print(headline.get_text())
+        try:  # есть блоки без span и текст с лишними символами :)
+            description = description.find("span").get_text().lstrip("\n")
+        except Exception as _:
+            description = description.get_text().lstrip("\n").lstrip()
+
+        print(number)
+        print(name)
+        print(description)
+        print(price)
+
+        print()
+
 else:
-    print(f'Ошибка: не удалось получить доступ к {url}, статус код: {response.status_code}')
+    print(f"Ошибка: не удалось получить доступ к {url}, статус код: {response.status_code}")
