5a05c522eeb7d86f0c49a59b313130ddcec65377
jonyks <jonyk-vs66@mail.ru>
2024-05-23 23:53:11 +0500

    Ничего

--- animation1.py
+++ animation1.py
@@ -8,31 +8,35 @@ from PySide6.QtSvg import QSvgRenderer
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        global pixmap
         super().__init__()
         self.title = "Анимация картинки SVG"
         self.setWindowTitle(self.title)
-    def create(self,filepath):
-        self.label = QLabel(self)
-        self.renderer = QSvgRenderer(filepath)
 
-        pixmap = self.render_svg(200, 100)
-        self.label.setPixmap(pixmap)
-        self.label.setStyleSheet("border: 2px solid white; border-radius: 10px;")
-        self.anim = QPropertyAnimation(self.label, b"pos")
-        self.anim.setStartValue(QPoint(self.width() - pixmap.width(), 0))
-        self.anim.setEndValue(QPoint(0, 0))
+
+
+        self.resize(1000, 1000)
+    def create(self, filepath, numb,i):
+        self.numb=numb
+        self.list=['label1','label2','label3']
+        self.renderer = QSvgRenderer(filepath)
+        self.list[i] = QLabel(self)
+        self.pixmap = self.render_svg(200, 100)
+        self.list[i].setPixmap(self.pixmap)
+        self.list[i].setStyleSheet("border: 2px solid white; border-radius: 10px;")
+        self.anim = QPropertyAnimation(self.list[i], b"pos")
+        self.anim.setStartValue(QPoint(self.width() - self.pixmap.width(), numb))
+        self.anim.setEndValue(QPoint(0, numb))
         self.anim.setEasingCurve(QEasingCurve.InOutQuad)
         self.anim.setDuration(2000)
         self.anim.start()
-        self.setCentralWidget(self.label)
-        self.resize(1000, 1000)
+        self.setCentralWidget(self.list[i])
+
 
         QTimer.singleShot(8000, self.start_hide_animation)  # Start hide animation after 8 seconds
 
     def start_hide_animation(self):
-        self.anim.setStartValue(QPoint(0, 0))
-        self.anim.setEndValue(QPoint(-pixmap.width(), 0))
+        self.anim.setStartValue(QPoint(0, self.numb))
+        self.anim.setEndValue(QPoint(-self.pixmap.width(), self.numb))
         self.anim.start()
 
     def render_svg(self, width, height):
@@ -48,13 +52,12 @@ class MainWindow(QMainWindow):
 
         return pixmap
 
-
-
 app = QApplication(sys.argv)
 
 w = MainWindow()
-# w.create('images\\volna.svg')
-w.create('images\\volna_mo.svg')
+w.create('images\\volna.svg',0,0)
+w.create('images\\volna_mo.svg',100,1)
+w.create('images\\volna_oz.svg',200,2)
 w.show()
 
 sys.exit(app.exec())
