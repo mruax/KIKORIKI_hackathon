5c794a4f51dfbfabf53177b8a6d7222eb96783d3
mruax <matvey.chernyakov@yandex.ru>
2024-05-23 21:44:37 +0500

    added kostylniy 2nd page

--- parser.py
+++ parser.py
@@ -111,6 +111,7 @@ if __name__ == "__main__":
     all_products = get_all_products()
     print(all_products)
     get_items(url=all_products[0], page_type=1)
+    get_items(url=all_products[0] + "/page/2", page_type=1)  # костыльно, но парсим вторую страницу
     get_items(url=all_products[1], page_type=2)
     get_items(url=all_products[2], page_type=3)
 
